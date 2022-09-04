435233d40d97997a9b3d307016a9ae1ce084b7a6
Сергей Фомин <xaoc615@gmail.com>
2022-09-04 16:53:44 +0300

    useState les-6

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,24 @@ import React from 'react';
 import './App.css';
 import Accordion from "./components/Accordion/Accordion";
 import {Rating} from "./components/Rating/Rating";
+import {OnOff} from "./components/OnOff/OnOff";
+import UncontrolledAccordion from "./components/Accordion/UncontrolledAccordion";
 
 function App() {
     return <div>
-        <AppTitle title = {'Hello'} />
-        <AppTitle title = {'My Friends'}/>
-        <Accordion titleValue = {'Menu'} collapsed={true}/>
-        <Accordion titleValue = {'Users'} collapsed={false}/>
-        <Rating value={0}/>
-        <Rating value={1}/>
-        <Rating value={2}/>
-        <Rating value={3}/>
-        <Rating value={4}/>
-        <Rating value={5}/>
+        <OnOff />
+        <UncontrolledAccordion titleValue={'Menu'}/>
+        <UncontrolledAccordion titleValue={'Users'}/>
+        {/*<AppTitle title = {'Hello'} />*/}
+        {/*<AppTitle title = {'My Friends'}/>*/}
+        {/*<Accordion titleValue = {'Menu'} collapsed={true}/>*/}
+        {/*<Accordion titleValue = {'Users'} collapsed={false}/>*/}
+        {/*<Rating value={0}/>*/}
+        {/*<Rating value={1}/>*/}
+        {/*<Rating value={2}/>*/}
+        {/*<Rating value={3}/>*/}
+        {/*<Rating value={4}/>*/}
+        {/*<Rating value={5}/>*/}
     </div>
 }
 type AppTitlePropsType = {
---
+++ src/components/OnOff/OnOff.tsx
@@ -0,0 +1,44 @@
+import React, {useState} from "react";
+type OnOffType = {
+    //on:boolean
+
+}
+export const OnOff = (props:OnOffType) => {
+    let [on,setOn] = useState(false)
+    const onStyle = {
+        width: '30px',
+        height: '20px',
+        border: '1px solid black',
+        display: 'inLine-block',
+        padding:'2px',
+        backgroundColor: on ? 'green' : 'white',
+    }
+    const offStyle = {
+        width: '30px',
+        height: '20px',
+        border: '1px solid black',
+        display: 'inLine-block',
+        marginLeft:'2px',
+        padding:'2px',
+        backgroundColor: on ? 'white' : 'red'
+    }
+    const indicatorStyle = {
+        width: '10px',
+        height: '10px',
+        borderRadius: '15px',
+        border:'1px solid black',
+        display: 'inLine-block',
+        marginLeft:'5px',
+        backgroundColor: on ? 'green' : 'red',
+
+
+    }
+
+    return(
+        <div>
+            <div style={onStyle} onClick={()=> {setOn(true)} }>On</div>
+            <div style={offStyle} onClick={()=> {setOn(false)} }>Off</div>
+            <div style={indicatorStyle}></div>
+        </div>
+    )
+}
