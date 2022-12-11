44e2f5ce07b99392021581ad06737c4e3fe506c6
Сергей Фомин <xaoc615@gmail.com>
2022-12-11 20:37:57 +0300

    useEffect  les 21

--- src/UseEffect.tsx
+++ src/UseEffect.tsx
@@ -24,3 +24,29 @@ Hello,{counter}
 </>
 }
 
+export const SetTimeoutExample = () => {
+    const [counter, setCounter] = useState(1)
+
+    console.log('SetTimeoutExample')
+
+
+    useEffect(() => {
+        setTimeout(()=>{
+            console.log('setTimeout')
+            document.title = counter.toString();
+        },1000)
+    },[counter])
+
+    useEffect(() => {
+        setInterval(()=>{
+            console.log('tik' + counter)
+           setCounter((state)=>state+1)
+        },1000)
+    },[])
+
+return<>
+Hello,{counter}
+    <button onClick={()=> setCounter(counter+1)}>+</button>
+</>
+}
+
